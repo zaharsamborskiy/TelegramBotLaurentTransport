8638cdde9cb3f25c0e058b59a7401f81e70c5629
zaharsamborskiy <117350706+zaharsamborskiy@users.noreply.github.com>
2024-09-26 22:59:32 +0700

    four

--- deploy.sh
+++ deploy.sh
@@ -6,10 +6,12 @@ ENV_FILE="./.env"
 pushd ~/TelegramBotLaurentTransport/ || exit
 
 # Переходим на ветку main
-git checkout dev
+#git checkout dev
+git fetch
 
 # Обновляем ветку main
-git pull origin dev
+#git pull origin dev
+git pull origin main
 
 # Останавливаем старые контейнеры микросервисов и запускаем новые, с обновлённым кодом
 docker compose -f docker-compose.yml --env-file $ENV_FILE down --timeout=60 --remove-orphans
