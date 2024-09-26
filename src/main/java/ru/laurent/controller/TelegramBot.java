33377f69911480c72ca9d87ac1c1eb633835c50c
zaharsamborskiy <117350706+zaharsamborskiy@users.noreply.github.com>
2024-09-26 16:46:45 +0700

    Reapply "Reapply "first""
    This reverts commit 42fff7ae1ea59aeccf0ba3cfef374ff27a560766.

---
+++ src/main/java/ru/laurent/controller/TelegramBot.java
@@ -0,0 +1,73 @@
+package ru.laurent.controller;
+
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import org.telegram.telegrambots.bots.TelegramWebhookBot;
+import org.telegram.telegrambots.meta.api.methods.BotApiMethod;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.methods.updates.SetWebhook;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
+import javax.annotation.PostConstruct;
+
+
+
+@Component
+@Log4j
+@RequiredArgsConstructor
+public class TelegramBot extends TelegramWebhookBot {
+    @Value("${bot.name}")
+    private String botName;
+    @Value("${bot.token}")
+    private String botToken;
+    @Value("${bot.uri}")
+    private String botUri;
+    private final UpdateProcessor updateProcessor;
+
+
+    @PostConstruct
+    public void init() {
+        updateProcessor.registerBot(this);
+        try {
+            var setWebhook = SetWebhook.builder()
+                    .url(botUri)
+                    .build();
+            this.setWebhook(setWebhook);
+        } catch (TelegramApiException e) {
+            log.error(e);
+        }
+    }
+
+    @Override
+    public String getBotUsername() {
+        return botName;
+    }
+
+    @Override
+    public String getBotToken() {
+        return botToken;
+    }
+
+    @Override
+    public BotApiMethod<?> onWebhookUpdateReceived(Update update) {
+        return null;
+    }
+
+    @Override
+    public String getBotPath() {
+        return "/update";
+    }
+
+    public void sendAnswerMessage(SendMessage message) {
+        try {
+            execute(message);
+        } catch (TelegramApiException e) {
+            log.error(e);
+        }
+    }
+}
